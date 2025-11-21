18874c9bc8725a4616cca90c0e723562f92c5bfb
John Mampilli <j.mampilli@imperial.ac.uk>
2025-11-21 17:00:54 +0000

    Updates to std::vector operator overloads

--- src/REAXFF/vector_utils.h
+++ src/REAXFF/vector_utils.h
@@ -2,7 +2,11 @@
 #define VECUTILS_H
 
 #include <vector>
-#include "crs_matrix.h"
+#include <algorithm>
+#include <functional>
+#include <cmath>
+#include <numeric>
+// #include "crs_matrix.h"
 
 template <typename T>
 std::vector<size_t> sort_permutation(const std::vector<T>& vec_to_sort) {
@@ -25,6 +29,15 @@ std::vector<T> operator+(const std::vector<T>& v1, const std::vector<T>& v2) {
     return result;
 }
 
+// template <typename T>
+// std::vector<T> operator+(const std::vector<T>& v1, const std::vector<T>& v2) {
+//     // Add vectors elementwise
+//     // Not checking vector sizes
+//     std::vector<T> result(v1.size());
+//     std::transform(v1.cbegin(), v1.cend(), v2.cbegin(), result.begin(), std::plus<>{}); // NOT GUARANTEED TO BE IN ORDER
+//     return result;
+// }
+
 template <typename T>
 std::vector<T> operator-(const std::vector<T>& v1, const std::vector<T>& v2) {
     // Subtract vectors elementwise
@@ -36,6 +49,15 @@ std::vector<T> operator-(const std::vector<T>& v1, const std::vector<T>& v2) {
     return result;
 }
 
+// template <typename T>
+// std::vector<T> operator-(const std::vector<T>& v1, const std::vector<T>& v2) {
+//     // Subtract vectors elementwise
+//     // Not checking vector sizes
+//     std::vector<T> result(v1.size());
+//     std::transform(v1.cbegin(), v1.cend(), v2.cbegin(), result.begin(), std::minus<>{}); // NOT GUARANTEED TO BE IN ORDER
+//     return result;
+// }
+
 template <typename T>
 std::vector<T> operator*(const T& scalar, const std::vector<T>& vector) {
     // Scale vector elementwise
@@ -45,6 +67,29 @@ std::vector<T> operator*(const T& scalar, const std::vector<T>& vector) {
     }
     return result;
 }
+
+template <typename T>
+double inner_product(const std::vector<T>& v1, const std::vector<T>& v2) {
+    // Not checking vector sizes
+    return std::inner_product(v1.cbegin(), v1.cend(), v2.cbegin(), 0.0);
+    // double result = 0.0;
+    // for (size_t i = 0; i < v1.size(); ++i) {
+    //     result += v1[i] * v2[i];
+    // }
+    // return result;
+}
+
+// template <typename T>
+// T operator*(const std::vector<T>& v1, const std::vector<T>& v2) {
+//     // '*' operator overload for vector-vector inner product
+//     return std::inner_product(v1.cbegin(), v1.cend(), v2.begin(), 0);
+// }
+
+template <typename T>
+double norm(const std::vector<T>& v1) {
+    return std::sqrt(inner_product(v1, v1));
+}
+
 
 // double operator*(const std::vector<double>& v1, const std::vector<double>& v2) {
 //     // '*' operator overload for vector-vector inner product
